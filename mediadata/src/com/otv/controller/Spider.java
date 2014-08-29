4377a11bf48aeb4c4611b58fcd3b65526602e986
janhway <janhway@gmail.com>
2014-08-29 22:37:50 +0800

    initial code.

--- mediadata/src/com/otv/controller/Spider.java
+++ mediadata/src/com/otv/controller/Spider.java
@@ -1,19 +1,14 @@
 package com.otv.controller;
 
-import java.io.IOException;
-import java.io.PrintWriter;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
 
-import com.otv.entity.ReturnMsg;
-import com.otv.entity.User;
+import com.otv.utils.OtvUtils;
 
 @Controller
 @RequestMapping("/spider")
@@ -21,19 +16,12 @@ public class Spider {
 
 	@RequestMapping(value = "/ottmedia", method = RequestMethod.POST, consumes = "application/json")
 	public void createOttMedia(HttpServletRequest req, HttpServletResponse rsp) {
-
 		
+		String httpContent = OtvUtils.readHttpContent(req);
 		
-		rsp.setContentType("text/plain");
-		PrintWriter out;
-		try {
-			out = rsp.getWriter();
-			out.println("ok");
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-			//rsp.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, "abcdefg");
-		}
+		System.out.print(httpContent);
+		
+		OtvUtils.writeHttpResponse(rsp, "ok");
 
 		return;
 	}
---
+++ mediadata/src/com/otv/utils/OtvUtils.java
@@ -0,0 +1,49 @@
+package com.otv.utils;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintWriter;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class OtvUtils {
+
+	static public String readHttpContent(HttpServletRequest req) {
+		String ret = null;
+		final int maxLen = 8192;
+		byte[] readBuf = new byte[maxLen];
+
+		try {
+			InputStream is = req.getInputStream();
+			int len = 0;
+			int offset = 0;
+			while (offset < maxLen
+					&& (len = is.read(readBuf, offset, maxLen - offset)) > 0) {
+				offset += len;
+			}
+
+			if (offset > 0) {
+				ret = new String(readBuf, "utf-8");
+			}
+
+		} catch (IOException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+
+		return ret;
+	}
+
+	static public void writeHttpResponse(HttpServletResponse rsp, String content) {
+		rsp.setContentType("text/plain");
+		PrintWriter out;
+		try {
+			out = rsp.getWriter();
+			out.println(content);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
